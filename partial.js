a637920941a8cf431398525daca04cf2c0786408
Ian <Obe_ty@yahoo.com>
2015-08-03 04:03:50 -0500

    had to convince git to delete files

--- partial.js
+++
@@ -1,9 +0,0 @@
-function partial(funct) {
-	var args = Array.prototype.slice.call(arguments, 1);
-	return function() {
-	var allArguments = args.concat(Array.prototype.slice.call(arguments));
-	return funct.apply(this, allArguments);
-	};
-}
-
-// partial(console.log.bind(console), 4)();
